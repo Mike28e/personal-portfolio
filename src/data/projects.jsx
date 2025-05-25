53bb3743ab35eed3b2de71e79f0723dd29f098b5
Mike28e <35285982+Mike28e@users.noreply.github.com>
2025-05-25 11:42:06 -0700

    updating projects

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,5 +1,22 @@
 export const projects = [
   {
+    title: 'Survey Data Processing Portal',
+    description:
+      'A web portal for creating and managing survey email campaigns, processing responses, generating reports, and visualizing data.',
+    tags: [
+      'Vue.js',
+      'Vuetify',
+      '.NET Core',
+      'GCP',
+      'Terraform',
+      'Apache Beam',
+      'Javascript',
+    ],
+    image: '/assets/images/confidential.png',
+    // github: '',
+    // demo: ''
+  },
+  {
     title: 'Luxury Car Rental Site',
     description:
       'A luxury car rental platform with booking options, site analytics, and chatbot integration.',
@@ -9,7 +26,7 @@ export const projects = [
     // demo: 'https://dycerentals.example.com',
   },
   {
-    title: 'Ai-Powered Smart Survey',
+    title: 'AI-Powered Smart Survey',
     description:
       'A smart survey platform that uses AI to analyze responses and probe for answers.',
     tags: ['React', 'Tailwind CSS', 'Next.js', 'Supabase', 'OpenRouter'],
